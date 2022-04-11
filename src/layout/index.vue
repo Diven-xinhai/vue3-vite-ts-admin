9649db78412f1aed22463880ccfeb0d3b8d6fae8
Diven <1205838869@qq.com>
2022-04-11 17:59:29 +0800

    无

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,11 +1,11 @@
 <!--
  * @Date: 2022-04-11 09:54:14
  * @LastEditors: YeKe
- * @LastEditTime: 2022-04-11 17:45:30
+ * @LastEditTime: 2022-04-11 17:57:08
  * @FilePath: \vue3-vite-ts-admin\src\layout\index.vue
 -->
 <template>
-  <div>
+  <div class="app-wrapper">
     <Sidebar />
     <AppMain />
   </div>
@@ -18,4 +18,10 @@ const envTitle = import.meta.env;
 console.log(envTitle);
 </script>
 
-<style scoped></style>
+<style scoped>
+.app-wrapper {
+  position: relative;
+  height: 100%;
+  width: 100%;
+}
+</style>
